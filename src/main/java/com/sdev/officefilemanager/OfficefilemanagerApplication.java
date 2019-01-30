4b6384c2937eff5b66e9ec1ce2c92fc8682aacd6
linux- local <sunilpoudel11@gmail.com>
2019-01-30 16:12:43 +0545

    file manager for office is created

---
+++ src/main/java/com/sdev/officefilemanager/OfficefilemanagerApplication.java
@@ -0,0 +1,14 @@
+package com.sdev.officefilemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfficefilemanagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OfficefilemanagerApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/com/sdev/officefilemanager/OfficefilemanagerApplicationTests.java
@@ -0,0 +1,17 @@
+package com.sdev.officefilemanager;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OfficefilemanagerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
